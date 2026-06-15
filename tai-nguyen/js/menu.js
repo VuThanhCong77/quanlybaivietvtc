9b6b36f7e48d852ae0a21caed7f91ad7678299bf
Vũ Thành Công <2025muahenamay@gmail.com>
2026-06-15 21:40:01 +0700

    Add files via upload
    Signed-off-by: Vũ Thành Công <2025muahenamay@gmail.com>

--- tai-nguyen/js/menu.js
+++ tai-nguyen/js/menu.js
@@ -11,3 +11,10 @@ function toggleMenu(){
     .toggle("active");
 
 }
+
+const btn = document.querySelector('.dropdown-btn');
+const menu = document.querySelector('.dropdown-menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
