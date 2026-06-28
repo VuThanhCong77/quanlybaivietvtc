d1e6dadf24ddb1d8cc6e74fd08957adb19db408d
Vũ Thành Công <2025muahenamay@gmail.com>
2026-06-28 11:30:04 +0700

    Update notification.js
    Signed-off-by: Vũ Thành Công <2025muahenamay@gmail.com>

--- notification.js
+++ notification.js
@@ -11,7 +11,7 @@ import {
 
 const vapidKey = "BIUs3ELW2tcEm8It0mD0OGrL1ChD5sBimbDXy3NDIkyUxYthLpxG8A2OD_ROaBUotCjURabPU94o07w47RjPwC4";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbzIkdtneKYjjk1jqsRsRrUkZ4EP0fXSZ5UiSjAbntfrKd5N7VDBlssQ7mVQvoyWV5iY/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyPLrpQYoZ_CrcJDyyR5OFHe2nABaBxguWv5j4MARnZVbOB2c07_V4apk_GrFhEp1YL/exec";
 
 // ===========================
 // CẬP NHẬT GIAO DIỆN
