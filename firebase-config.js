e77d00fcfd32cf8bb8424af2fbc3b7b2e7e50470
Vũ Thành Công <2025muahenamay@gmail.com>
2026-06-28 13:45:49 +0700

    Update firebase-config.js
    Signed-off-by: Vũ Thành Công <2025muahenamay@gmail.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,15 @@
 // firebase-config.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+import { getMessaging }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+
+import {
+    getFirestore
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvzI0JvNuR5bNaHDT7_oP-5-n72ve4fuU",
@@ -17,3 +24,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const messaging = getMessaging(app);
+
+const app = initializeApp(firebaseConfig);
+
+export const messaging = getMessaging(app);
+
+export const db = getFirestore(app);
