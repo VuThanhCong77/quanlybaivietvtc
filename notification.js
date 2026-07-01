90c634d43599fea128e67a7414122bb59479c304
Vũ Thành Công <2025muahenamay@gmail.com>
2026-07-01 13:07:43 +0700

    Update notification.js
    Signed-off-by: Vũ Thành Công <2025muahenamay@gmail.com>

--- notification.js
+++ notification.js
@@ -65,12 +65,12 @@ function capNhatTrangThai(permission) {
     switch (permission) {
 
         case "granted":
-            btn.innerHTML = "✅ Đang nhận thông báo";
+            btn.innerHTML = "✅ Đang nhận";
             btn.classList.add("subscribed");
             break;
 
         case "denied":
-            btn.innerHTML = "🚫 Đã chặn thông báo";
+            btn.innerHTML = "🚫 Đã chặn";
             btn.classList.add("blocked");
             break;
 
