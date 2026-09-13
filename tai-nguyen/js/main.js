9aa4c03677aaeae9e4b0a4e4ac338a9529f5f4f3
Vũ Thành Công <2025muahenamay@gmail.com>
2026-09-13 10:57:30 +0700

    Update main.js
    Signed-off-by: Vũ Thành Công <2025muahenamay@gmail.com>

--- tai-nguyen/js/main.js
+++ tai-nguyen/js/main.js
@@ -28,6 +28,46 @@ async function taiMenu(){
     el.innerHTML = html;
 }
 
+async function loadComponent(id, file){
+
+    try{
+
+        const response = await fetch(file);
+
+        if(!response.ok){
+
+            throw new Error(
+                "Lỗi tải component: " + file
+            );
+
+        }
+
+        const html = await response.text();
+
+        document.getElementById(id)
+        .innerHTML = html;
+
+    }catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+    loadComponent(
+        "menu",
+        "https://vuthanhcong77.github.io/quanlybaivietvtc/thanh-phan/menu.html"
+    );
+
+  }
+);
+
+
 
 
 async function napThanhTimKiem() {
@@ -73,4 +113,3 @@ document.addEventListener(
     "DOMContentLoaded",
     napThanhTimKiem
 );
-
